1f4ca51a26c8ead0dc85a8ea2ac7fa24cd67d52b
AugustoCVS <guty.vertolis@gmail.com>
2023-11-25 04:09:21 -0300

    feat: call hook and adjusts

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,59 +1,60 @@
 import React from "react";
 import { View, Text, Pressable } from "react-native";
-import { Fontisto, MaterialIcons, FontAwesome5 } from "@expo/vector-icons";
+import {
+  Fontisto,
+  MaterialIcons,
+  FontAwesome5,
+} from "@expo/vector-icons";
 
 import { useDashboard } from "./hook";
 import { CTA } from "src/components/CTA";
 import { DashboardHeader } from "./components/Header";
+import { ScrollView } from "native-base";
+import { ModalEmergencyInfo } from "./components/ModalEmergencyInfo";
 
 export const Dashboard: React.FC = () => {
-  const { actions } = useDashboard();
+  const { refs, actions } = useDashboard();
 
   return (
-    <View className="flex bg-white-default h-full w-full">
-      <DashboardHeader />
+    <>
+      <ScrollView
+        className="flex bg-white-default h-full w-full"
+        showsVerticalScrollIndicator={false}
+      >
+        <DashboardHeader modalEmergencyInfoRef={refs.modalEmergencyInfoRef} />
 
-      <View className="flex flex-col p-8">
-        <View className="w-full bg-orange-default rounded-md">
-          <Text className="text-xl text-white-default font-bold text-center pt-4">
-            Informações para Emergências
-          </Text>
+        <View className="flex flex-col p-8">
+          <View className="w-full flex flex-col items-center justify-center">
+            <CTA
+              label="Histórico"
+              icon={<FontAwesome5 name="notes-medical" size={26} color="#F6931F" />}
+              onPress={actions.test}
+            />
 
-          <View className="flex flex-col items-start p-4">
-            <Text className="text-md text-white-default font-bold text-center pt-4">
-              Tipo Sanguíneo:
-            </Text>
-            <Text className="text-md text-white-default font-bold text-center pt-4">
-              Alergias:
-            </Text>
-            <Text className="text-md text-white-default font-bold text-center pt-4">
-              Medicamentos:
-            </Text>
+            <CTA
+              label="Consultas"
+              icon={<Fontisto name="doctor" size={26} color="#F6931F" />}
+              onPress={actions.test}
+            />
+
+            <CTA
+              label="Exames"
+              icon={<MaterialIcons name="science" size={28} color="#F6931F" />}
+              onPress={actions.test}
+            />
+
+            <CTA
+              label="Receitas"
+              icon={
+                <FontAwesome5 name="laptop-medical" size={22} color="#F6931F" />
+              }
+              onPress={actions.test}
+            />
           </View>
         </View>
+      </ScrollView>
 
-        <View className="w-full flex flex-col items-center justify-center">
-          <CTA
-            label="Consultas"
-            icon={<Fontisto name="doctor" size={26} color="#F6931F" />}
-            onPress={actions.test}
-          />
-
-          <CTA
-            label="Exames"
-            icon={<MaterialIcons name="science" size={28} color="#F6931F" />}
-            onPress={actions.test}
-          />
-
-          <CTA
-            label="Receitas"
-            icon={
-              <FontAwesome5 name="laptop-medical" size={22} color="#F6931F" />
-            }
-            onPress={actions.test}
-          />
-        </View>
-      </View>
-    </View>
+      <ModalEmergencyInfo modalRef={refs.modalEmergencyInfoRef} />
+    </>
   );
 };
