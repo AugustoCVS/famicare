a29e4e56c564df52250374dd38d1c4c7a750570d
AugustoCVS <guty.vertolis@gmail.com>
2023-11-25 03:16:45 -0300

    feat: create appointments screen

---
+++ src/screens/Appointments/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export const Appointments: React.FC = () => {
+
+  return (
+    <View className="flex items-center justify-center h-full">
+      <Text>Consultas</Text>
+    </View>
+  );
+};
