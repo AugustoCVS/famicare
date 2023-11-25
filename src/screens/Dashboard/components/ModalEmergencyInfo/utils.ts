59bbc49327f6ce2adba30d8eb6797f50cd566309
AugustoCVS <guty.vertolis@gmail.com>
2023-11-25 04:07:47 -0300

    feat: create utils

---
+++ src/screens/Dashboard/components/ModalEmergencyInfo/utils.ts
@@ -0,0 +1,32 @@
+export const UserEmergencyInfo = [
+  {
+    id: 1,
+    label: "Tipo Sanguíneo: ",
+    response: "A+"
+  },
+  {
+    id: 2,
+    label: "Alergias: ",
+    response: "Nenhuma alergia conhecida."
+  },
+  {
+    id: 3,
+    label: "Medicamentos: ",
+    response: "Não"
+  },
+  {
+    id: 4,
+    label: "Doenças Crônicas: ",
+    response: "Não"
+  },
+  {
+    id: 5,
+    label: "Hospital de Preferência: ",
+    response: "Não especificado."
+  },
+  {
+    id: 6,
+    label: "Contatos de Emergência: ",
+    response: "11952980373"
+  }
+];
