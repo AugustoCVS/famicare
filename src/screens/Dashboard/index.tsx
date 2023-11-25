6d6936796782c5bbb08dcfce88447662b2dd9240
AugustoCVS <guty.vertolis@gmail.com>
2023-11-25 03:17:05 -0300

    feat: update dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -13,24 +13,46 @@ export const Dashboard: React.FC = () => {
     <View className="flex bg-white-default h-full w-full">
       <DashboardHeader />
 
-      <View className="w-full p-8 flex flex-col items-center justify-center">
-        <CTA
-          label="Consultas"
-          icon={<Fontisto name="doctor" size={26} color="#F6931F" />}
-          onPress={actions.test}
-        />
+      <View className="flex flex-col p-8">
+        <View className="w-full bg-orange-default rounded-md">
+          <Text className="text-xl text-white-default font-bold text-center pt-4">
+            Informações para Emergências
+          </Text>
 
-        <CTA
-          label="Exames"
-          icon={<MaterialIcons name="science" size={26} color="#F6931F" />}
-          onPress={actions.test}
-        />
+          <View className="flex flex-col items-start p-4">
+            <Text className="text-md text-white-default font-bold text-center pt-4">
+              Tipo Sanguíneo:
+            </Text>
+            <Text className="text-md text-white-default font-bold text-center pt-4">
+              Alergias:
+            </Text>
+            <Text className="text-md text-white-default font-bold text-center pt-4">
+              Medicamentos:
+            </Text>
+          </View>
+        </View>
 
-        <CTA
-          label="Receitas"
-          icon={<FontAwesome5 name="book-medical" size={26} color="#F6931F" />}
-          onPress={actions.test}
-        />
+        <View className="w-full flex flex-col items-center justify-center">
+          <CTA
+            label="Consultas"
+            icon={<Fontisto name="doctor" size={26} color="#F6931F" />}
+            onPress={actions.test}
+          />
+
+          <CTA
+            label="Exames"
+            icon={<MaterialIcons name="science" size={28} color="#F6931F" />}
+            onPress={actions.test}
+          />
+
+          <CTA
+            label="Receitas"
+            icon={
+              <FontAwesome5 name="laptop-medical" size={22} color="#F6931F" />
+            }
+            onPress={actions.test}
+          />
+        </View>
       </View>
     </View>
   );
