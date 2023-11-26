a193ab0c0df557698e9499f9f2f895880f6a7fa5
AugustoCVS <guty.vertolis@gmail.com>
2023-11-26 17:11:14 -0300

    feat: add icon and change date style

--- src/screens/Dashboard/components/AppointmentsSummary/index.tsx
+++ src/screens/Dashboard/components/AppointmentsSummary/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 
 import * as T from "./types";
 import { View, Text } from "react-native";
@@ -10,11 +11,27 @@ export const AppointmentsSummary: React.FC<T.AppointmentsSummaryProps> = ({
   type,
 }) => {
   return (
-    <View className="flex flex-col items-start justify-between bg-white-100 w-[220px] flex-1 mx-4 p-4 rounded-lg border border-orange-default">
-      <Text className="mb-2 text-lg text-bold text-gray-default">{day}</Text>
-      <Text className="mb-1 text-bold">{doctor}</Text>
-      <Text className="mb-1 text-bold">{hospital}</Text>
-      <Text className="mb-1 text-bold">{type}</Text>
+    <View className="flex flex-col items-start justify-between bg-white-100 w-[220px] flex-1 mx-4 mb-8 p-4 rounded-lg border border-orange-default">
+      <View className="bg-orange-200 items-center justify-center rounded-xl px-4 mb-2">
+      <Text className="text-lg text-bold text-gray-default">
+        {day}
+      </Text>
+      </View>
+      <Text className="mb-1 text-bold">
+        <Ionicons name="medical-sharp" size={12} color="#F6931F" />
+        {' '}
+        {doctor}
+      </Text>
+      <Text className="mb-1 text-bold">
+        <Ionicons name="medical-sharp" size={12} color="#F6931F" />
+        {' '}
+        {hospital}
+      </Text>
+      <Text className="mb-1 text-bold">
+        <Ionicons name="medical-sharp" size={12} color="#F6931F" />
+        {' '}
+        {type}
+      </Text>
     </View>
   );
 };
