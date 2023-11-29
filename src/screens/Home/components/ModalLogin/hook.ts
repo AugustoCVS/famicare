a7409aebeaace464bdc358458def2b59d5864450
AugustoCVS <guty.vertolis@gmail.com>
2023-11-29 15:03:12 -0300

    refactor: consume backend for login

--- src/screens/Home/components/ModalLogin/hook.ts
+++ src/screens/Home/components/ModalLogin/hook.ts
@@ -4,13 +4,12 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 
 import * as T from "./types";
 import * as U from "./utils";
-import { FIREBASE_AUTH } from "auth/FirebaseConfig";
-import AsyncStorage  from "@react-native-async-storage/async-storage";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useNavigation } from "@react-navigation/native";
 import { StackTypes } from "src/routes/stack.routes";
+import { AuthServices } from "src/services/auth";
 
 export const useModalLogin = () => {
-  const auth = FIREBASE_AUTH;
   const [secureTextEntry, setSecureTextEntry] = useState(true);
   const [loading, setLoading] = useState(false);
 
@@ -18,7 +17,13 @@ export const useModalLogin = () => {
 
   const toast = useToast();
 
-  const showToast = ({title, error}: {title: string, error: boolean }): void => {
+  const showToast = ({
+    title,
+    error,
+  }: {
+    title: string;
+    error: boolean;
+  }): void => {
     toast.show({
       title: title,
       duration: 3000,
@@ -32,23 +37,29 @@ export const useModalLogin = () => {
   };
 
   const forgetPassword = (): void => {
-    showToast({title: "Funcionalidade em desenvolvimento!", error: true})
-  }
+    showToast({ title: "Funcionalidade em desenvolvimento!", error: true });
+  };
 
   const saveUserTokenOnStorage = async (token: string): Promise<void> => {
     await AsyncStorage.setItem("@userToken", token);
-  }
+  };
 
   const handleSignUp = async (FormData: T.useLoginProps): Promise<void> => {
     try {
       setLoading(true);
       await U.signInSchema.validate(FormData, { abortEarly: false });
-      const response = await signInWithEmailAndPassword(auth, FormData.email, FormData.password);
-      await saveUserTokenOnStorage(response.user.uid);
+
+      const response = await AuthServices.loginFamily({
+        email: FormData.email,
+        password: FormData.password,
+      });
+
+      await saveUserTokenOnStorage(response.token);
+
       navigation.navigate("Dashboard");
     } catch (error) {
-     showToast({title: "E-mail ou senha inválidos", error: true});
-     console.log(error)
+      showToast({ title: "E-mail ou senha inválidos", error: true });
+      console.log(error);
     } finally {
       setLoading(false);
     }
