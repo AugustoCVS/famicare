b3d4dbaacad473154d99182e8b1104eb347cc1e2
AugustoCVS <guty.vertolis@gmail.com>
2023-11-29 15:02:44 -0300

    feat: create interfaces for login request and response

--- src/services/interfaces/auth.ts
+++ src/services/interfaces/auth.ts
@@ -10,3 +10,12 @@ export interface RegisterReponse {
     name: string;
     email: string;
 }
+
+export interface LoginRequest {
+    email: string;
+    password: string;
+}
+
+export interface LoginResponse {
+    token: string;
+}
